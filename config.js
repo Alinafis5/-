c732e3701a14cd7508aa1ead08ab95679ad9ac0e
Alinafis5 <alinafisalinafis1@gmail.com>
2024-08-30 12:09:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -14,52 +14,52 @@ global.authFile = `MysticSession`;
 global.isBaileysFail = false
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['573012482597', '💫 Mystic - OfcDiego', true],
-  ['59175655964', '💫 Mystic - By Alba070503 💫', true],
-  ['5493794297363', '💫 Mystic - Gabriel Ofc', true], 
-  ['393518398856', '💫 Mystic - Tester Fabri115💫', true],
-  ['212781160203', '💫 Mystic - Saad 💫', true], 
-  ['5217778573223', '💫 Mystic - Sarethh 💫', true],
-  ['593968585283'],
-  ['5492266466080'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50497150165'],
-  ['51906662557'],
-  ['573183650526'], 
-  ['5217441298510'], 
-  ['5217294888993'],
-  ['595992611272'],
-  ['5219992843892'],
-  ['5493795319019'],
-  ['573147616444'], 
-  ['5219991402134'],
-  ['5219996125657'],
-  ['5492215034412'],
-  ['5492964650915'],
-  ['5218444937407']
+  ['212681274130', '👑 ALI NAFIS 👑', true],
+  ['212681274130', '💫  💫', true],
+  ['212681274130', '💫  💫', true],
+  ['212681274130', '💫  💫', true],
+  ['212681274130', '💫  💫', true],
+  ['212681274130', '💫  💫', true],
+  ['212681274130', '💫  💫', true],
+  ['212681274130', '💫  💫', true],
+  ['212681274130', '💫  💫', true],
+  ['212681274130', '💫  💫', true],
+  ['212681274130', '💫  💫', true], 
+  ['212681274130', '💫  💫', true],
+  ['212681274130', '💫  💫', true], 
+  ['212681274130', '💫  💫', true],
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130'], 
+  ['212681274130'],  
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130'], 
+  ['212681274130'], 
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130'], 
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130'],
+  ['212681274130']
 ];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['212681274130'];
+global.prems = ['212681274130'];
 
 global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
+global.author = 'Ali Nafis - Bot';
+global.wm = 'Ali Nafis - Bot';
+global.titulowm = 'Ali Nafis - Bot';
+global.titulowm2 = `Ali Nafis - Bot`
+global.igfg = 'Ali Nafis - Bot';
 global.wait = '*_[ ⏳ ] Cargando..._*';
 
 global.imagen1 = fs.readFileSync('./src/assets/images/menu/languages/es/menu.png');
@@ -79,16 +79,16 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nThe Mystic - Bot`;
-global.gt = 'The Mystic - Bot';
-global.mysticbot = 'The Mystic - Bot';
-global.channel = 'https://whatsapp.com/channel/0029Vaein6eInlqIsCXpDs3y';
-global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
+global.wm2 = `${dia} ${fecha}\nAli Nafis - Bot`;
+global.gt = 'Ali Nafis - Bot';
+global.mysticbot = 'Ali Nafis - Bot';
+global.channel = '❤️❤️';
+global.md = 'https://github.com/';
+global.mysticbot = 'https://github.com/';
 global.waitt = '*_[ ⏳ ] Cargando..._*';
 global.waittt = '*_[ ⏳ ] Cargando..._*';
 global.waitttt = '*_[ ⏳ ] Cargando..._*';
-global.nomorown = '5219993404349';
+global.nomorown = '212681274130';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
